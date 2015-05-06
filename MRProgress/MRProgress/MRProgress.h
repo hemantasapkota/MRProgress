1710ae6ba42dfd3130e189ffcbc1c30b0eecfa0a
Hemanta Sapkota <laex.pearl@gmail.com>
2015-05-06 23:16:16 +1000

    Carthage support for https://github.com/mrackwitz/MRProgress

---
+++ MRProgress/MRProgress/MRProgress.h
@@ -0,0 +1,33 @@
+//
+//  MRProgress.h
+//  MRProgress
+//
+//  Created by Hemanta Sapkota on 6/05/2015.
+//  Copyright (c) 2015 Hemanta Sapkota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MRProgress.
+FOUNDATION_EXPORT double MRProgressVersionNumber;
+
+//! Project version string for MRProgress.
+FOUNDATION_EXPORT const unsigned char MRProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRProgress/PublicHeader.h>
+
+#import "MRActivityIndicatorView.h"
+#import "MRBlurView.h"
+#import "UIImage+MRImageEffects.h"
+#import "MRCircularProgressView.h"
+#import "MRIconView.h"
+#import "MRMessageInterceptor.h"
+#import "MRNavigationBarProgressView.h"
+#import "MRProgressOverlayView.h"
+#import "MRWeakProxy.h"
+
+#import "MRActivityIndicatorView.h"
+#import "MRCircularProgressView.h"
+#import "MRIconView.h"
+#import "MRNavigationBarProgressView.h"
+#import "MRProgressOverlayView.h"
